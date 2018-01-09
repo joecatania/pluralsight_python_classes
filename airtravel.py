e9ac7ba8980bb643b8afadbef2f2ebb327f7c0cb
joecatania <joecatania0@gmail.com>
2018-01-08 21:04:17 -0500

    update

--- airtravel.py
+++ airtravel.py
@@ -3,7 +3,16 @@
 class Flight:
 
     def __init__(self, number):
+        if not number[:2].isalpha():
+            raise ValueError("no airline code in {}".format(number))
+        if not number[:2].isupper():
+            raise ValueError("invalid airline code {}".format(number))
+        if not (number[2:].isdigit() and int(number[2:]) <= 9999):
+            raise ValueError("invalid route number {}".format(number))
         self._number = number
 
     def number(self):
         return self._number
+
+    def airline(self):
+        return self._number[:2]
